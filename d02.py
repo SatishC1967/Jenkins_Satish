318b8eb196265bbc0617bdfe2880087fbb7fd93f
SatishC1967 <53887336+SatishC1967@users.noreply.github.com>
2020-11-03 15:02:17 +0530

    Update d02.py

--- d02.py
+++ d02.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello(): 
 	return "welcome to the Oracle web app with python in DevSecOPs By Satish Chauhan !!"
 @app.route('/oracle')
-def hello():	
+def hello1():	
 	return "Welcome to ORacle Containerization process !!"
 if __name__ == "__main__": 
 	app.run(host ='0.0.0.0', port = 5000, debug = True)

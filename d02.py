67a9cfd0264058c2e214703d0002e8e7de22b01a
SatishC1967 <53887336+SatishC1967@users.noreply.github.com>
2020-11-04 09:55:17 +0530

    Update d02.py

--- d02.py
+++ d02.py
@@ -6,6 +6,6 @@ def hello():
 	return "welcome to the Oracle web app with python in DevSecOPs By Satish Chauhan !!"
 @app.route('/oracle')
 def hello1():	
-	return "Welcome to ORacle Containerization process !!"
+	return "Welcome to ORacle Containerization process Satish Chauhan!!"
 if __name__ == "__main__": 
 	app.run(host ='0.0.0.0', port = 5000, debug = True)
